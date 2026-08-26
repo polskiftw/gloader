eec821cc83bb1af39c4464770747a8545c4e2aa4
polskiftw <polskiftw@gmail.com>
2026-08-26 17:52:54 -0400

    Compile Infinite Angler chat command support

--- tests/InfiniteAnglerCompile/TerrariaStubs.cs
+++ tests/InfiniteAnglerCompile/TerrariaStubs.cs
@@ -1,3 +1,11 @@
+namespace Microsoft.Xna.Framework
+{
+    public struct Color
+    {
+        public static Color Yellow => new Color();
+    }
+}
+
 namespace Terraria
 {
     public class Main
@@ -16,9 +24,49 @@ namespace Terraria
     }
 }
 
+namespace Terraria.Chat
+{
+    public sealed class ChatMessage
+    {
+        public string Text { get; set; }
+        public bool IsConsumed { get; private set; }
+
+        public ChatMessage(string text)
+        {
+            Text = text;
+        }
+
+        public void Consume()
+        {
+            IsConsumed = true;
+        }
+    }
+
+    public class ChatCommandProcessor
+    {
+        public void ProcessIncomingMessage(ChatMessage message, int clientId)
+        {
+        }
+    }
+
+    public static class ChatHelper
+    {
+        public static void SendChatMessageToClient(
+            Terraria.Localization.NetworkText text,
+            Microsoft.Xna.Framework.Color color,
+            int playerId)
+        {
+        }
+    }
+}
+
 namespace Terraria.Localization
 {
     public class NetworkText
     {
+        public static NetworkText FromLiteral(string text)
+        {
+            return new NetworkText();
+        }
     }
 }
