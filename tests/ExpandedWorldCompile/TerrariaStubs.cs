b091206ea3bd9f69eaaeb13f34d467cdd9c13231
polskiftw <polskiftw@gmail.com>
2026-09-01 13:59:42 -0400

    Stub Rectangle for Temple compile coverage

--- tests/ExpandedWorldCompile/TerrariaStubs.cs
+++ tests/ExpandedWorldCompile/TerrariaStubs.cs
@@ -14,6 +14,22 @@ namespace Microsoft.Xna.Framework
         public int Y;
         public Point(int x, int y) { X = x; Y = y; }
     }
+
+    public struct Rectangle
+    {
+        public int X;
+        public int Y;
+        public int Width;
+        public int Height;
+
+        public Rectangle(int x, int y, int width, int height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+    }
 }
 
 namespace Terraria
@@ -62,6 +78,7 @@ namespace Terraria
         public static void CreateNewWorld() { }
         public static void clearWorld() { }
         public static void GenerateWorld() { }
+        public static void makeTemple(int x, int y) { }
 
         public static void TileRunner(
             int i,
